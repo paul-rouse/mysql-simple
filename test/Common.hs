237a85c553956bb2ff60b7d2f75558a5d73fce5a
Vekhir <134215107+Vekhir@users.noreply.github.com>
2024-01-09 16:30:50 +0100

    Support bytestring 0.11.1.0
    Data.ByteString.Builder defines an instance Show Builder since bytestring 0.11.1.0

--- test/Common.hs
+++ test/Common.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE StandaloneDeriving #-}
 {-# options_ghc -fno-warn-orphans #-}
 
@@ -7,8 +8,10 @@ import Data.ByteString.Builder as BS
 import Database.MySQL.Simple.Param
 
 -- Declare some (orphan) instances needed for test specs
+#if !MIN_VERSION_bytestring(0,11,1)
 instance Show BS.Builder where
   show = show . BS.toLazyByteString
+#endif
 
 instance Eq BS.Builder where
   a == b = BS.toLazyByteString a == BS.toLazyByteString b
